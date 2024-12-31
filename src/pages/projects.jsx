d8764ce411f3c314ed823e48b7f2bf21e49f1a50
John David Gaspar <johndavidgaspar1107@gmail.com>
2024-12-31 10:36:12 +0800

    Added Project page

---
+++ src/pages/projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+        <div>
+            <h2>Project</h2>
+            <p>Here are some of my personal projects</p>
+        </div>
+    </div>
+  )
+};
+
+export default Projects;
