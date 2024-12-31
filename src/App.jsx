65935e3fdaf8301cb165bfd86266774970766838
John David Gaspar <johndavidgaspar1107@gmail.com>
2024-12-31 10:32:57 +0800

    Added an '@' in my import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navigation from '@/components/navigation.jsx';
 import Home from '@/pages/home.jsx';
 import About from '@/pages/about.jsx';
 import Qualification from '@/pages/qualification';
-import Skills from './pages/skills';
+import Skills from '@/pages/skills';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
