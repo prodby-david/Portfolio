2c2c28f5e81b0186eb907ab40e0d03989b98d304
John David Gaspar <johndavidgaspar1107@gmail.com>
2024-12-31 10:22:10 +0800

    Removed the project import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Qualification from '@/pages/qualification';
 import Skills from './pages/skills';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Projects from './pages/projects';
 
 function App() {
 
